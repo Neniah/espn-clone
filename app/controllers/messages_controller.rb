f33475b115514d62792166cfca2203f5aecd446b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-10-24 13:24:57 +0200

    edit and delete for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
 
-  before_action :find_message, only: [:show, :edit, :update, :delete]
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
 
   def index
     @messages = Message.all.order('created_at DESC')
@@ -20,6 +20,22 @@ class MessagesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
   end
 
   private
